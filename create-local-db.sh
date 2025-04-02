f6d39acbd442064cd2f04b1b95ca53c41e1106bd
David Blanco <david.lucas@maeztra.com>
2025-04-02 09:13:55 -0300

    create DB

---
+++ create-local-db.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Start or create PostgreSQL container
+if ! docker ps -a --format '{{.Names}}' | grep -q '^tech-savior-psql$'; then
+    docker run --name tech-savior-psql -p 5432:5432 -e POSTGRES_PASSWORD=education -d postgres
+    echo "Waiting for PostgreSQL to start..."
+    sleep 5
+else
+    echo "PostgreSQL container already exists."
+    docker start tech-savior-psql
+fi
+
+# Wait until PostgreSQL is ready
+until docker exec tech-savior-psql pg_isready -U postgres; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 5
+done
+
+# Create database inside the container
+docker exec -it tech-savior-psql psql -U postgres -c "CREATE DATABASE techsavior;"
+
+# test if the DB exists:
+docker exec -it tech-savior-psql psql -U postgres -c "\l"
+
+
+
+
+
+
+
+
