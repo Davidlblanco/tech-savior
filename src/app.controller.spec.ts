3c562a379872bad0c4a8412ae32b0f721e56c8c7
David Blanco <davidlucasblanco@gmail.com>
2025-04-04 19:05:16 -0300

    apiUrl

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "TechSaivior is health!"', () => {
+      expect(appController.getHealth()).toBe('TechSaivior is health!');
     });
   });
 });
